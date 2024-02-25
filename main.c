dfbb6441fa9cbf6314b921f772d52844c6fa25bf
yairMar1 <156588104+yairMar1@users.noreply.github.com>
2024-02-25 17:18:38 +0200

    Add files via upload
    25.2.24 17:18

--- main.c
+++ main.c
@@ -12,7 +12,7 @@ int main() {
 
     do {
           if (scanf("%d", &choice) != 1) {
-            printf("Invalid input. Please enter a number.");
+            //printf("Invalid input. Please enter a number.");
             // Clear input buffer
             while (getchar() != '\n');
             continue;
@@ -20,10 +20,10 @@ int main() {
 
         switch (choice) {
             case 1:
-                printf("Enter how many word do you mean to enter: ");
+                //printf("Enter how many word do you mean to enter: ");
                 int x;
                 scanf("%d%*c", &x); // קריאה למשתנה וניקוי תו זבל
-                printf("Enter the strings to insert separated by spaces: ");
+                //printf("Enter the strings to insert separated by spaces: ");
                 scanf("%[^\n]s", data); // Change format to %[^\n]
                 char *token = strtok(data, " ");
                 while (token != NULL && x>0) {
@@ -33,55 +33,57 @@ int main() {
                 }
                 break;
             case 2:
-                printf("Enter the index to insert: ");
+                //printf("Enter the index to insert: ");
                 scanf("%d", &index);
-                printf("Enter the string to insert: ");
+                //printf("Enter the string to insert: ");
                 scanf(" %[^\n]s", data);
                 StrList_insertAt(list, data, index);
                 break;
             case 3:
-                printf("List contents:");
+                //printf("List contents:");
                 if(list==NULL){return 0;}
                 else{StrList_print(list);}
                 break;
             case 4:
-                printf("Length of the list: %zu", StrList_size(list));
+            printf("%zu", StrList_size(list));
+                //printf("Length of the list: %zu", StrList_size(list));
                 break;
             case 5:
-                printf("Enter the index: ");
+                //printf("Enter the index: ");
                 scanf("%d", &index);
                 StrList_printAt(list, index);
                 break;
             case 6:
-                printf("Total number of characters in the list: %d", StrList_printLen(list));
+            printf("%d", StrList_printLen(list));
+                //printf("Total number of characters in the list: %d", StrList_printLen(list));
                 break;
             case 7:
-                printf("Enter the string to count: ");
+                //printf("Enter the string to count: ");
                 scanf(" %[^\n]s", data);
-                printf("Occurrences of \"%s\": %d", data, StrList_count(list, data));
+                //printf("Occurrences of \"%s\": %d", data, StrList_count(list, data));
                 break;
             case 8:
-                printf("Enter the string to remove: ");
+                //printf("Enter the string to remove: ");
                 scanf(" %[^\n]s", data);
                 StrList_remove(list, data);
                 break;
             case 9:
-                printf("Enter the index to remove: ");
+                //printf("Enter the index to remove: ");
                 scanf("%d", &index);
                 StrList_removeAt(list, index);
                 break;
             case 10:
                 StrList_reverse(list);
-                printf("List reversed.");
+                //printf("List reversed.");
                 break;
             case 11:
                 StrList_free(list);
                 list = StrList_alloc();
-                printf("List deleted.");
+                //printf("List deleted.");
                 break;
             case 12:
                 StrList_sort(list);
-                printf("List sorted.");
+                //printf("List sorted.");
                 break;
             case 13:
                 if (StrList_isSorted(list))
@@ -90,10 +92,10 @@ int main() {
                     printf("False");
                 break;
             case 0:
-                printf("Exiting program.");
+                //printf("Exiting program.");
                 break;
             default:
-                printf("Invalid choice!");
+                //printf("Invalid choice!");
         }
     }
     while (choice != 0);
